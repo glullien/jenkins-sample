2a46ae12abf83ab91af6a080a57dcd84e2eb33dd
Gurvan Lullien <gurvan.lullien@gmail.com>
2022-02-18 11:48:17 +0100

    Fix Sonar

--- src/main/java/fr/orsys/samples/App.java
+++ src/main/java/fr/orsys/samples/App.java
@@ -1,13 +1,19 @@
 package fr.orsys.samples;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+	private static final Logger logger = LogManager.getLogger();
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        logger.info( "Hello World!" );
     }
 }
+
